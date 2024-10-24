8d33afdbfa95004f6fe67f3bef8b593c66562d36
Marshal Marks <marshmarks@gmail.com>
2024-10-24 12:57:33 -0500

    first commit

---
+++ src/pages/ElementGallery.js
@@ -0,0 +1,25 @@
+function ElementGallery() {
+    return (
+        <>
+            <div className="Card softElevated">
+                <h3>Soft Text Card</h3>
+                <p>This is a card with a header and text. It also has soft edges.</p>
+            </div>
+
+            <div className="Card hardElevated">
+                <h3>Hard Text Card</h3>
+                <p>This is a card with a header and text. It also has hard edges.</p>
+            </div>
+
+            <button className="Card hardElevated" onClick={() => console.log("click!")}>
+                Hard Button
+            </button>
+
+            <button className="Card softElevated" onClick={() => console.log("click!")}>
+                Soft Button
+            </button>
+        </>
+    );
+}
+
+export default ElementGallery;
