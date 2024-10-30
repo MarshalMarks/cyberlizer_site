2e4b632405b5b2ac88d15b2c74f4c5a8ca08afaf
Marshal Marks <marshmarks@gmail.com>
2024-10-30 14:31:22 -0500

    Added hr element styling and test page formatting

--- src/pages/ElementGallery.js
+++ src/pages/ElementGallery.js
@@ -1,33 +1,41 @@
 function ElementGallery() {
     return (
         <>
-            <div className="Card softElevated">
-                <h3>Soft Text Card</h3>
-                <p>This is a card with a header and text. It also has soft edges.</p>
+            <h1 className="header">Neumorphic Elements</h1>
+
+            <hr className="spacer"></hr>
+            
+            <div className="elementContainer">
+                <div className="Card softElevated">
+                    <h3>Soft Text Card</h3>
+                    <p>This is a card with a header and text. It also has soft edges.</p>
+                </div>
+
+                <div className="Card hardElevated">
+                    <h3>Hard Text Card</h3>
+                    <p>This is a card with a header and text. It also has hard edges.</p>
+                </div>
+
+                <button className="Card hardElevated" onClick={() => console.log("click!")}>
+                    Hard Button
+                </button>
+
+                <button className="Card softElevated" onClick={() => console.log("click!")}>
+                    Soft Button
+                </button>
+
+                <button className="Card hardElevated disabled" onClick={() => console.log("click!")}>
+                    Hard Disabled Button
+                </button>
+
+                <button className="Card softElevated disabled" onClick={() => console.log("click!")}>
+                    Soft Disabled Button
+                </button>
             </div>
 
-            <div className="Card hardElevated">
-                <h3>Hard Text Card</h3>
-                <p>This is a card with a header and text. It also has hard edges.</p>
-            </div>
+            <hr className="spacer"></hr>
 
-            <button className="Card hardElevated" onClick={() => console.log("click!")}>
-                Hard Button
-            </button>
-
-            <button className="Card softElevated" onClick={() => console.log("click!")}>
-                Soft Button
-            </button>
-
-            <button className="Card hardElevated disabled" onClick={() => console.log("click!")}>
-                Hard Disabled Button
-            </button>
-
-            <button className="Card softElevated disabled" onClick={() => console.log("click!")}>
-                Soft Disabled Button
-            </button>
-
-            <hr></hr>
+            <p className="footerText"><i>Clay Marks 2024</i></p>
         </>
     );
 }
